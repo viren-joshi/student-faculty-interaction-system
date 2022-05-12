b2b095368d64e83167532ceb91fe9c212fb98c35
Viren Joshi <viren.joshi403@gmail.com>
2022-05-12 17:16:55 +0530

    api fixing cuz i dont have anything else to do

--- app.py
+++ app.py
@@ -27,6 +27,14 @@ app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root:password@localhost:3
 #           if the process is not successful then return {'message': 'Error (error description if possible'}
 
 
+success_dict = {
+    "message":"Success"
+}
+
+fail_dict = {
+    "message":"Fail"
+}
+
 @app.route('/studentreg',methods=['POST'])
 def registration():
     data=request.json
@@ -36,43 +44,48 @@ def registration():
     password=data['password']
     svv_id=data['svv_id']
     year=data['year']
-    department_name=data['department_name']
-    dept_id=(db.engine.execute(f'select dept_id from department where department_name="{department_name}"'))
-    for i in dept_id:
-        dept_id = (i[0])
-        break
-    db.engine.execute(f'insert into students_details(st_name,st_email,st_phone,st_pass,st_svv_id,st_year,dept_id) values("{name}","{email}",{phone},"{password}",{svv_id},"{year}",{dept_id});')
-    return "Executed"
+    dept_id=data['dept_id']
+    try:
+        db.engine.execute(f'insert into students_details(st_name,st_email,st_phone,st_pass,st_svv_id,st_year,dept_id) values("{name}","{email}",{phone},"{password}",{svv_id},"{year}",{dept_id});')
+    except:
+        return fail_dict
+    return success_dict
+
 
 @app.route('/login',methods=['POST'])
 def login():
     data=request.json
     email=data['email']
     password=data['password']
-    check=db.engine.execute(f'select st_id from students_details where st_email="{email}" and st_pass="{password}";')
+    try:
+        check=db.engine.execute(f'select st_id from students_details where st_email="{email}" and st_pass="{password}";')
+    except :
+        return fail_dict
     l=0
-    response = {}
     for i in check:
         l+=1
         loggedin_id=i[0]
     if l>0:
-        response['stu_id'] = str(loggedin_id)
-        response['message'] = 'Success'
-        
-        return response
+        return {
+            "message":"Success",
+            "login_id":str(loggedin_id)
+            }
     else:
-        return "False"
+        return fail_dict
 
 @app.route('/postQuestion',methods=['POST'])
 def postQuestion():
     data = request.json
     question = data['question']
     stu_id = data['stu_id']
-    fac_id = data['faculty_id']
     topic = data['topic']
     date = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-    db.engine.execute(f'insert into question(question,fac_id,stu_id,date,topic) VALUES ("{question}",{fac_id},{stu_id},"{date}","{topic}")')
-    return "Executed"
+    fac_id=data['fac_id']    
+    try:
+        db.engine.execute(f'insert into question(question,fac_id,stu_id,date,topic) VALUES ("{question}",{fac_id},{stu_id},"{date}","{topic}")')
+    except:
+        return fail_dict
+    return success_dict
 
 # TODO : In API above take faculty id as input, remove the faculty name part.
 
@@ -81,28 +94,41 @@ def postQuestion():
 def getAllQuestions():
     data = request.json
     stu_id = data['stu_id']
-    result = db.engine.execute(f'select * from question,answer where stu_id={stu_id} and question.quest_id=answer.quest_id;')
+    #SELECT question.*, answer.answer_id,answer.answer FROM question LEFT JOIN answer ON question.quest_id = answer.quest_id WHERE question.stu_id = 1;
+    try:
+        result = db.engine.execute(f'select question.*, answer.answer_id,answer.answer from question LEFT OUTER JOIN answer on question.quest_id = answer.quest_id where stu_id = {stu_id};')
+    except:
+        return fail_dict
     response = {}
-    i=1
-    for each in result:
-        response.update({f'{i}.': list(each)})
-        i+=1
+    response["message"] = "Success"
+    if not result :
+        response["isThere"] = "No Questions"
+    else :
+        i=1
+        for each in result:
+            response.update({f'{i}': dict(zip(result.keys(),each))})
+            i+=1
+        response['isThere'] = "Yes"
     return response
 
 @app.route('/getAllQuestionsTeacher',methods=['POST'])
 def getAllQuestionsTeacher():
     data = request.json
     fac_id = data['fac_id']
-    result = db.engine.execute(f'select * from question where fac_id={fac_id} and status=false;')
+    try :
+        result = db.engine.execute(f'select * from question where fac_id={fac_id} and status=false;')
+    except:
+        return fail_dict
     response = {}
     i=1
     for each in result:
-        response.update({f'{i}.': list(each)})
+        response.update({f'{i}': dict(zip(result.keys(),each))})
         i+=1
     if response :
-        response['message'] = 'Success'
+        response['isThere'] = "No Questions"
     else :
-        response['message'] = 'Error'
+        response['isThere'] = "Yes"
+    response['message'] = 'Success'
     return response
 
 @app.route('/postAnswer',methods=['POST'])
@@ -110,33 +136,46 @@ def postAnswer():
     data = request.json
     quest_id=data['quest_id']
     answer = data['answer']
-    db.engine.execute(f'insert into answer(quest_id,answer) values ({quest_id},"{answer}");')
-    db.engine.execute(f'update question set status=true where quest_id={quest_id};')
-    return "MAZA AA GAYA"
+    try :
+        db.engine.execute(f'insert into answer(quest_id,answer) values ({quest_id},"{answer}");')
+        db.engine.execute(f'update question set status=true where quest_id={quest_id};')
+    except:
+        return fail_dict
+    return success_dict
 
 @app.route('/teacherLogin',methods=['POST'])
 def teacherLogin():
     data=request.json
     email=data['email']
     password=data['password']
-    check=db.engine.execute(f'select fac_id from faculty where fac_email="{email}" and fac_pass="{password}";')
+    try:
+        check=db.engine.execute(f'select fac_id from faculty where fac_email="{email}" and fac_pass="{password}";')
+    except:
+        return fail_dict
     l=0
+    response = {}
     for i in check:
         l+=1
         loggedin_id=i[0]
     if l>0:
-        return str(loggedin_id)
+        response["message"] = "Success"
+        response["teach_id"] = str(loggedin_id)
+        return response
     else:
-        return "False"    
+        return fail_dict   
 
 @app.route('/getAllTeachers',methods=['POST'])
 def getAllTeachers():
     data = request.json
-    result = db.engine.execute(f'select fac_id, fac_name from faculty;')
+    key = data['key']
+    try:
+        result = db.engine.execute(f'select fac_id, fac_name from faculty;')
+    except:
+        return fail_dict
     response = {}
     i=1
     for each in result:
-        response.update({f'{i}.': list(each)})
+        response.update({f'{i}': dict(zip(result.keys(),each))})
         i+=1
     return response        
 # msg is successful or error key - value
